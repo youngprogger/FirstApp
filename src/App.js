a96857faac99f6c6909572bd615ffc46cc842792
youngprogger <morevzhenya20@mail.ru>
2021-04-27 15:01:38 +0300

    Revert "done"
    This reverts commit cc0ada845084ffa95ebdc67095168c20851afac9.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <div>
-        <div><h1>Задачи на неделю:</h1></div>
           <MyTodoList/>
         </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
@@ -18,39 +25,21 @@ function App() {
 class MyTodoList extends React.Component {
   state = {
     tasks: [{
-      id: 123,
-      name: 'Visit my grandma',
-      description: 'On friday i should visit my grandma and spent time with she',
-      completed: true
-    },
-    {
       id: 1234,
-      name: 'Dedline at my ux/ui design courses',
-      description: 'I should add my new work on Behance',
-      completed: false
-    },
-    {
-      id: 12345,
-      name: 'Buy Tesla shares',
-      description: 'This is a perfect moment to buy Tesla shares this weekend',
-      completed: false
-    },
-    {
-      id: 12346,
-      name: 'Go to the Gym',
-      description: 'I nevet went to the gym after my illness. It is time to get in shape!',
+      name: 'Name of the task',
+      description: 'What needs to be done',
       completed: true
     },
     {
-      id: 123467,
-      name: 'Coursework',
-      description: 'It is time to start my coursework',
-      completed: false
+      id: 1235,
+      name: 'Name of the task',
+      description: 'What needs to be done',
+      completed: true
     },
     {
-      id: 1234678,
-      name: 'Work deadline',
-      description: 'It is time to start my coursework',
+      id: 1236,
+      name: 'Name of the task',
+      description: 'What needs to be done',
       completed: true
     }
   ]
@@ -82,10 +71,10 @@ const TaskComplete = ({ id, name, description, completed }) => {
 
   return (
     <div id={id}>
-        <h4 className="task">{name}</h4>
-        <p className="task">{description}</p>
-        <p className="task">{completed.toString()}</p>
-        <button className="button1" onClick={handleClick}>Done</button>
+        <h4>{name}</h4>
+        <p>{description}</p>
+        <p>{completed.toString()}</p>
+        <button onClick={handleClick}/>
     </div>
   )
 }
